769680b6b3f7e31bf524647acaebd4240c7bcdf0
gonzalespaulb <76889345+gonzalespaulb@users.noreply.github.com>
2023-01-17 20:12:21 -0700

    need to change ids

---
+++ src/Scheduler/utils/employeeList.js
@@ -0,0 +1,326 @@
+export const allEmployees = [
+    {
+        name: "Kacey", 
+        id: 000, 
+    }, 
+    {
+        name: "Shatz", 
+        id: 000, 
+    }, 
+    {
+        name: "Cal U", 
+        id: 000, 
+    }, 
+    {
+        name: "Renee", 
+        id: 000, 
+    }, 
+    {
+        name: "Cuz", 
+        id: 000, 
+    }, 
+    {
+        name: "Evan", 
+        id: 000, 
+    }, 
+    {
+        name: "Liam", 
+        id: 000, 
+    }, 
+    {
+        name: "Trysten", 
+        id: 000, 
+    }, 
+    {
+        name: "Anelise", 
+        id: 000, 
+    }, 
+    {
+        name: "Ben Hill", 
+        id: 000, 
+    }, 
+    {
+        name: "Josiah", 
+        id: 000, 
+    }, 
+    {
+        name: "Veronica", 
+        id: 000, 
+    }, 
+    {
+        name: "Jo R", 
+        id: 000, 
+    }, 
+    {
+        name: "Jeremiah", 
+        id: 000, 
+    }, 
+    {
+        name: "Ryan B", 
+        id: 000, 
+    }, 
+    {
+        name: "Jaden K", 
+        id: 000, 
+    }, 
+    {
+        name: "Trevor Johnston", 
+        id: 000, 
+    }, 
+    {
+        name: "Matias", 
+        id: 000, 
+    }, 
+    {
+        name: "Jose", 
+        id: 000, 
+    }, 
+    {
+        name: "Alvaro", 
+        id: 000, 
+    }, 
+    {
+        name: "Anton", 
+        id: 000, 
+    }, 
+    {
+        name: "Marissa", 
+        id: 000, 
+    }, 
+    {
+        name: "Valentina", 
+        id: 000, 
+    }, 
+    {
+        name: "Taylor A", 
+        id: 000, 
+    }, 
+    {
+        name: "Thomas G", 
+        id: 000, 
+    }, 
+    {
+        name: "Charles", 
+        id: 000, 
+    }, 
+    {
+        name: "Sebastian F", 
+        id: 000, 
+    }, 
+    {
+        name: "Sebastian G", 
+        id: 000, 
+    }, 
+    {
+        name: "Porter", 
+        id: 000, 
+    }, 
+    {
+        name: "Greyson", 
+        id: 000, 
+    }, 
+    {
+        name: "Julia", 
+        id: 000, 
+    }, 
+    {
+        name: "Calvin", 
+        id: 000, 
+    }, 
+    {
+        name: "Ben", 
+        id: 000, 
+    }, 
+    {
+        name: "Nic", 
+        id: 000, 
+    }, 
+    {
+        name: "Topher", 
+        id: 000, 
+    }, 
+    {
+        name: "Adam", 
+        id: 000, 
+    }, 
+    {
+        name: "Thomas B", 
+        id: 000, 
+    }, 
+    {
+        name: "Abby", 
+        id: 000, 
+    }, 
+    {
+        name: "Mel", 
+        id: 000, 
+    }, 
+    {
+        name: "Sam", 
+        id: 000, 
+    }, 
+    {
+        name: "Paul", 
+        id: 000, 
+    }, 
+    {
+        name: "Jeff Thomas", 
+        id: 000, 
+    }, 
+    {
+        name: "Steven F", 
+        id: 000, 
+    }, 
+    {
+        name: "Brian S", 
+        id: 000, 
+    }, 
+    {
+        name: "Lyric", 
+        id: 000, 
+    }, 
+    {
+        name: "Dillon", 
+        id: 000, 
+    }, 
+    {
+        name: "Jake", 
+        id: 000, 
+    }, 
+    {
+        name: "Tyler Ganci", 
+        id: 000, 
+    }, 
+    {
+        name: "Curtis", 
+        id: 000, 
+    }, 
+    {
+        name: "Grant", 
+        id: 000, 
+    }, 
+    {
+        name: "Alberto", 
+        id: 000, 
+    }, 
+    {
+        name: "Steven L", 
+        id: 000, 
+    }, 
+    {
+        name: "Jesus", 
+        id: 000, 
+    }, 
+    {
+        name: "Lizzeth", 
+        id: 000, 
+    }, 
+    {
+        name: "Kyle", 
+        id: 000, 
+    }, 
+    {
+        name: "Aiden", 
+        id: 000, 
+    }, 
+    {
+        name: "Alex", 
+        id: 000, 
+    }, 
+    {
+        name: "Ben F", 
+        id: 000, 
+    }, 
+    {
+        name: "Parker", 
+        id: 000, 
+    }, 
+    {
+        name: "Lucy", 
+        id: 000, 
+    }, 
+    {
+        name: "Daniela", 
+        id: 000, 
+    }, 
+    {
+        name: "Dayven", 
+        id: 000, 
+    }, 
+    {
+        name: "Izzer", 
+        id: 000, 
+    }, 
+    {
+        name: "Taylor T", 
+        id: 000, 
+    }, 
+    {
+        name: "Blake", 
+        id: 000, 
+    }, 
+    {
+        name: "Sarah", 
+        id: 000, 
+    }, 
+    {
+        name: "Nico", 
+        id: 000, 
+    }, 
+    {
+        name: "Blair", 
+        id: 000, 
+    }, 
+    {
+        name: "Adrian", 
+        id: 000, 
+    }, 
+    {
+        name: "Kate", 
+        id: 000, 
+    }, 
+    {
+        name: "Diego", 
+        id: 000, 
+    }, 
+    {
+        name: "Sean B", 
+        id: 000, 
+    }, 
+    {
+        name: "Logan", 
+        id: 000, 
+    }, 
+    {
+        name: "Rhett", 
+        id: 000, 
+    }, 
+    {
+        name: "Brody", 
+        id: 000, 
+    }, 
+    {
+        name: "Brandon", 
+        id: 000, 
+    }, 
+    {
+        name: "Bob", 
+        id: 000, 
+    }, 
+    {
+        name: "Blake S", 
+        id: 000, 
+    }, 
+    {
+        name: "Kyle C", 
+        id: 000, 
+    }, 
+    {
+        name: "Jaden R", 
+        id: 000, 
+    }, 
+    {
+        name: "Christian", 
+        id: 000, 
+    }, 
+]
