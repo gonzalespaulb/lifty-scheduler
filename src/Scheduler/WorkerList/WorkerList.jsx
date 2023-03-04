ccb307c22140f587460991655bab0cdc0ed11abb
gonzalespaulb <76889345+gonzalespaulb@users.noreply.github.com>
2023-03-03 20:12:12 -0700

    refactored workerlist logic

--- src/Scheduler/WorkerList/WorkerList.jsx
+++ src/Scheduler/WorkerList/WorkerList.jsx
@@ -14,6 +14,8 @@ const WorkerList = () => {
   let allWorkers = [];
 
   for (const lift of liftList) {
+    
+    // NOTE ------------------------------------------------------------------ ASSIGNS WORKER'S STATION POSITION
 
     const designationPosition = (worker) => {
       if (lift?.top?.includes(worker)) {
@@ -29,48 +31,68 @@ const WorkerList = () => {
       }
     };
 
-    if (lift.top === undefined) {
-      const liftWorkerList = [lift.bottom].flat();
+    // NOTE ------------------------------------------------------------------ CHECKS IF STATION POSITION EXISTS
+
+    const checkStation = (stationPositions) => {
+      let liftWorkerList = stationPositions.filter((station) => {
+        return station !== undefined;
+      });
+
+      liftWorkerList = liftWorkerList.flat();
 
       for (const worker of liftWorkerList) {
         worker.supervisor = lift.supervisor;
         worker.designation = designationPosition(worker);
-
         allWorkers.push(worker);
       }
-      continue;
-    }
+    };
 
-    if (lift.mid === undefined) {
-      const liftWorkerList = [lift.top, lift.bottom].flat();
+    checkStation([lift.top, lift.mid, lift.bottom]);
 
-      for (const worker of liftWorkerList) {
-        worker.supervisor = lift.supervisor;
-        worker.designation = designationPosition(worker);
+    // if (lift.top === undefined) {
+    //   const liftWorkerList = [lift.bottom].flat();
 
-        allWorkers.push(worker);
-      }
-      continue;
-    }
+    //   for (const worker of liftWorkerList) {
+    //     worker.supervisor = lift.supervisor;
+    //     worker.designation = designationPosition(worker);
 
-    const liftWorkerList = [lift.top, lift.mid, lift.bottom].flat();
+    //     allWorkers.push(worker);
+    //   }
+    //   continue;
+    // }
 
-    for (const worker of liftWorkerList) {
-      worker.supervisor = lift.supervisor;
-      worker.designation = designationPosition(worker);
-      allWorkers.push(worker);
-    }
+    // if (lift.mid === undefined) {
+    //   const liftWorkerList = [lift.top, lift.bottom].flat();
+
+    //   for (const worker of liftWorkerList) {
+    //     worker.supervisor = lift.supervisor;
+    //     worker.designation = designationPosition(worker);
+
+    //     allWorkers.push(worker);
+    //   }
+    //   continue;
+    // }
+
+    /*
+      Checks if the station position exists and if it doesnt will exclude it from the new filtered array. 
+    */
+
+    // const liftWorkerList = [lift.top, lift.mid, lift.bottom].flat();
+
+    // for (const worker of liftWorkerList) {
+    //   worker.supervisor = lift.supervisor;
+    //   worker.designation = designationPosition(worker);
+    //   allWorkers.push(worker);
+    // }
   }
 
-  for (const nonLift of nonLiftList) {
-    for (const worker of nonLift.workers) {
-      worker.supervisor = "Greg Green";
-      worker.designation = nonLift.name;
-      allWorkers.push(worker);
-    }
-  }
-
-  const workersToRender = allWorkers.flat();
+  // for (const nonLift of nonLiftList) {
+  //   for (const worker of nonLift.workers) {
+  //     worker.supervisor = "Greg Green";
+  //     worker.designation = nonLift.name;
+  //     allWorkers.push(worker);
+  //   }
+  // }
 
   const [openInfo, setOpenInfo] = useState(false);
   const [activeInfo, setActiveInfo] = useState(null);
@@ -86,9 +108,9 @@ const WorkerList = () => {
       }
     };
 
-    return workersToRender.map((worker, i) => {
+    return allWorkers.flatMap((worker, i) => {
       return (
-        <Worker openInfo={openInfo} activeInfo={activeInfo} index={i}>
+        <Worker openInfo={openInfo} activeInfo={activeInfo} index={i} key={i}>
           <WorkerContainer>
             <Name>{worker.name}</Name>
             <CrossIcon
