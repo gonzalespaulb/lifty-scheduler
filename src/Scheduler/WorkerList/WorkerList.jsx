606e26f5ec18cd3e9ed5a341bba7f5274356a152
gonzalespaulb <76889345+gonzalespaulb@users.noreply.github.com>
2023-03-20 20:57:01 -0600

    refactored render workers

--- src/Scheduler/WorkerList/WorkerList.jsx
+++ src/Scheduler/WorkerList/WorkerList.jsx
@@ -14,7 +14,6 @@ const WorkerList = () => {
   let allWorkers = [];
 
   for (const lift of liftList) {
-    
     // NOTE ------------------------------------------------------------------ ASSIGNS WORKER'S STATION POSITION
 
     const designationPosition = (worker) => {
@@ -48,51 +47,17 @@ const WorkerList = () => {
     };
 
     checkStation([lift.top, lift.mid, lift.bottom]);
-
-    // if (lift.top === undefined) {
-    //   const liftWorkerList = [lift.bottom].flat();
-
-    //   for (const worker of liftWorkerList) {
-    //     worker.supervisor = lift.supervisor;
-    //     worker.designation = designationPosition(worker);
-
-    //     allWorkers.push(worker);
-    //   }
-    //   continue;
-    // }
-
-    // if (lift.mid === undefined) {
-    //   const liftWorkerList = [lift.top, lift.bottom].flat();
-
-    //   for (const worker of liftWorkerList) {
-    //     worker.supervisor = lift.supervisor;
-    //     worker.designation = designationPosition(worker);
-
-    //     allWorkers.push(worker);
-    //   }
-    //   continue;
-    // }
-
-    /*
-      Checks if the station position exists and if it doesnt will exclude it from the new filtered array. 
-    */
-
-    // const liftWorkerList = [lift.top, lift.mid, lift.bottom].flat();
-
-    // for (const worker of liftWorkerList) {
-    //   worker.supervisor = lift.supervisor;
-    //   worker.designation = designationPosition(worker);
-    //   allWorkers.push(worker);
-    // }
   }
 
-  // for (const nonLift of nonLiftList) {
-  //   for (const worker of nonLift.workers) {
-  //     worker.supervisor = "Greg Green";
-  //     worker.designation = nonLift.name;
-  //     allWorkers.push(worker);
-  //   }
-  // }
+  // NOTE ------------------------------------------------------------------ NON LIFT PERSONEL TO BE RENDERED
+
+  for (const nonLift of nonLiftList) {
+    for (const worker of nonLift.workers) {
+      worker.supervisor = "Greg Green";
+      worker.designation = nonLift.name;
+      allWorkers.push(worker);
+    }
+  }
 
   const [openInfo, setOpenInfo] = useState(false);
   const [activeInfo, setActiveInfo] = useState(null);
