f7a8128805eaacb34c73e5992a6a0adfaa20c480
gonzalespaulb <76889345+gonzalespaulb@users.noreply.github.com>
2023-09-07 15:30:45 -0600

    Scheduler Restart

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import "./App.css";
-import Scheduler from "./Scheduler/Scheduler";
 import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Scheduler />
+          
       </BrowserRouter>
     </div>
   );
